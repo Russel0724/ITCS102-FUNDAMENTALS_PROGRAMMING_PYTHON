fcc2ba8ff7ae374738c8971cc7890c0e731a7c2b
Russel0724 <russelglenntiangco@gmail.com>
2025-08-11 16:55:20 +0800

    Codechallenge#1_ITCS102

---
+++ codechallenge#1 2.py
@@ -0,0 +1,3 @@
+name = input("State Your Name: ")
+
+print("\t\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t\t HI \t\t\t*\n\t\t\t*\t\t\t\t", name, "\t\t\t*\n\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t*")
