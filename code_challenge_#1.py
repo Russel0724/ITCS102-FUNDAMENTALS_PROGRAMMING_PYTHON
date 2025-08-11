a24b3908804ffeb9317a9ac902e317c0f5576818
Russel0724 <russelglenntiangco@gmail.com>
2025-08-11 14:31:05 +0800

    Delete code_challenge_#1.py

--- code_challenge_#1.py
+++
@@ -1,3 +0,0 @@
-name = input("State Your Name: ")
-
-print("\t\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t\t HI \t\t\t*\n\t\t\t\t\t*\t\t", name, "\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t*")
