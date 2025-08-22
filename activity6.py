8e9e5961d56a0190972bcf77cfafa82063d11bf5
Russel0724 <224033409+Russel0724@users.noreply.github.com>
2025-08-22 16:12:24 +0800

    calculator

---
+++ activity6.py
@@ -0,0 +1,14 @@
+x = eval(input("Type Your Number(1-100): "))
+y = eval(input("Type Your Number(1-100): "))
+l = x + y
+u = x - y
+m = x * y
+i = x / y
+
+print("The sum of", x, "and", y,"is", l)
+print("The difference of", x, "and" ,y, "is", u)
+print("The product of", x, "and" ,y, "is", m)
+print("The quotient of", x, "and" ,y, "is", i)
+print(x, "exponent by", y, "is" ,x**y)
+print("The remainder of", x, "and" ,y, "is", x%y)
+print("The floor division of", x, "and" ,y, "is", x//y)
