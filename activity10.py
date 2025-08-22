49d79dad3e091006a508209d20ae7c63cd33c8bc
Russel0724 <224033409+Russel0724@users.noreply.github.com>
2025-08-22 17:18:29 +0800

    jeepney fare

---
+++ activity10.py
@@ -0,0 +1,16 @@
+#comments
+#jepney fare
+#user would input their name
+#if user is student , student discount must be applied
+#20% discount on student, if not student no discount
+
+name = input("Input your name ---> ")
+isStudent = input("Are you a student (Yes / No) ---> ")
+fare = eval(input("bayad ---> "))
+
+if isStudent.lower() == "yes" :
+    discount = fare * .2 
+    new_fare = fare - discount
+    print("Hi", name, "student discount is ",discount, "Discounted fare is ", new_fare)
+else:
+    print("Sorry", name, "you are not eligible for student discount")
