27242d931772ddab29b2916be3de80b3ca2fa620
Russel0724 <russelglenntiangco@gmail.com>
2025-10-13 11:02:27 +0800

    christmas tree itez

---
+++ cc13.py
@@ -0,0 +1,51 @@
+#Christmas Tree
+for a in range(2,6,1):
+    for b in range(0,7):
+        print(" ",end=" ")
+    for c in range(6,a,-1):
+        print(" ",end=" ")
+    for d in range(3,a):
+        print("*",end=" ")
+    for e in range(2,a,1):
+        print("*",end=" ")
+    print()    
+for f in range(1,3,1):
+    for g in range(0,7):
+        print(" ",end=" ")
+    for h in range(0 - 1,f,1):
+        print(" ",end=" ")
+    for i in range(2,f,-1):
+        print("*",end=" ")
+    for j in range(3,f,-1):
+        print("*",end=" ")
+    print()    
+for k in range(2,9,1):
+    for l in range(12,k,-1):
+        print(" ",end=" ")
+    for m in range(2,k,1):
+        print("*",end=" ")
+    for n in range(1,k):
+        print("*",end=" ")
+    print()
+for o in range(2,10,1):
+    for p in range(12,o,-1):
+        print(" ",end=" ")
+    for q in range(1,o):
+        print("*",end=" ")
+    for r in range(2,o):
+        print("*",end=" ")
+    print()
+for s in range(2,13,1):
+    for t in range(12,s,-1):
+        print(" ",end=" ")
+    for u in range(1,s):
+        print("*",end=" ")
+    for v in range(2,s):
+        print("*",end=" ")
+    print()
+for w in range(1,7):
+    for x in range(1,9):
+        print(" ",end=" ")
+    for y in range(1,9):
+        print("",end="*")
+    print()
