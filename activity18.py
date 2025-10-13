77154db3cd5fd3c2b837d3543f1efb27d1b2cc5c
Russel0724 <russelglenntiangco@gmail.com>
2025-10-13 11:03:48 +0800

    right triangle numbers

---
+++ activity18.py
@@ -0,0 +1,9 @@
+for x in range(1,11,1):
+    for y in range(1,11,1):
+        print(y, end=' ')
+    print()
+
+for i in range(1,11,1):
+    for o in range(1,i,1):
+        print(o, end=' ')
+    print()
