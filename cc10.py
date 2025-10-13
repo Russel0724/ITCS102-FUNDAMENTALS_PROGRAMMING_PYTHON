8abfa940ffe00ac02a5092e100631186fcb83253
Russel0724 <russelglenntiangco@gmail.com>
2025-10-13 10:26:53 +0800

    Delete cc10.py

--- cc10.py
+++
@@ -1,8 +0,0 @@
-for m in range(1,11,1):
-    for g in range(10,m,-1):
-        print(" ", end=' ')
-    for t in range(1,m,1):
-        print("*", end=' ')
-    for r in range(1,m,1):
-        print("*", end=' ')
-    print()
