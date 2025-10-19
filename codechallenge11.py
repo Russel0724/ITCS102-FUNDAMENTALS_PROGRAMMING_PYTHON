88cb1c162f06597bd2cd7e7f9cf0c891ef35d45b
Russel0724 <russelglenntiangco@gmail.com>
2025-10-19 08:19:52 +0800

    Diamond

---
+++ codechallenge11.py
@@ -0,0 +1,17 @@
+print("\t\t *", end=" ")
+for g in range(1,11,1):
+    for r in range(10,g,-1):
+        print(" ", end=" ")
+    for m in range(1,g,1):
+        print("*", end=" ")
+    for t in range(1,g,1):
+        print("*", end=" ")
+    print()
+for g in range(1,11,1):
+    for r in range(1,g,1):
+        print(" ", end=" ")
+    for m in range(10,g,-1):
+        print("*", end=" ")
+    for t in range(10,g,-1):
+        print("*", end=" ")
+    print()
