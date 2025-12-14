97a24bf4e93be72b119feedbcdcd88ea97d94957
Russel0724 <russelglenntiangco@gmail.com>
2025-12-14 19:12:46 +0800

    Add files via upload

---
+++ activity25_1.py
@@ -0,0 +1,8 @@
+def ac1():
+    name = input("What is your name: ")
+def ac2():
+    age = eval(input("What is your age: "))
+def ac3():
+    address = input("Where do you live: ")
+def ac4():
+    sex = input("Male/Female -> ")
